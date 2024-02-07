40e9feb1a83dc14c21add473a0fba612e2983b6d
pedromoura1 <55463700+pedromoura1@users.noreply.github.com>
2024-02-06 21:26:12 -0300

    Revert "Updates on Splash"
    This reverts commit 42b54b26511ed508f06e4e5812ce61904f70b5ac.

--- Foodie/App.tsx
+++ Foodie/App.tsx
@@ -52,16 +52,15 @@ function Section({ children, title }: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
