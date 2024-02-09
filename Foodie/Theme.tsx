12ad28a7a1d4757c4001da8289d534b102535283
alaniv <ivoalani@outlook.com>
2024-02-08 21:22:25 -0300

    Colors del Theme

---
+++ Foodie/Theme.tsx
@@ -0,0 +1,16 @@
+const Theme = {
+    colors : {
+        PRIMARY_1 : '#2196F3',
+        PRIMARY_2 : '#6EB8F5',
+        PRIMARY_3 : '#9ACEF8',
+        SECONDARY_1 : '#F37F21',
+        SECONDARY_2 : '#F8A82E',
+        SECONDARY_3 : '#FBBF35',
+        NEUTRAL_1 : '#121212',
+        NEUTRAL_2 : '#484848',
+        NEUTRAL_3 : '#797979',
+        NEUTRAL_4 : '#FFFFFF',
+    }
+};
+
+export default Theme;
--- Foodie/src/components/LandingScreen.tsx
+++ Foodie/src/components/LandingScreen.tsx
@@ -1,4 +1,5 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Theme from '../../Theme';
 
 const LandingScreen = () => {
   return (
@@ -8,9 +9,17 @@ const LandingScreen = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Soy la landing!</Text>
+      <Text style={styles.text}>Soy la landing!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    text : {
+      color : Theme.colors.PRIMARY_1,
+      paddingHorizontal: 24,
+      fontSize: 48
+    },
+  });
 
 export default LandingScreen;
