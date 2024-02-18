bbf02f8ec4d600ebe6e3cf88323d1bf18c825aac
pedromoura1 <55463700+pedromoura1@users.noreply.github.com>
2024-02-18 13:56:38 -0300

    Implementacion d ProgressBar

--- Foodie/src/components/NewRecipeScreen1.tsx
+++ Foodie/src/components/NewRecipeScreen1.tsx
@@ -1,22 +1,16 @@
+// NewRecipeScreen1.tsx
+
 import React, { useState } from 'react';
-import {
-  PermissionsAndroid,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { CommonStyle, Theme } from '../../Theme';
 import { PrimaryButton } from './PrimaryButton';
 import { SmallButton } from './SmallButton';
-import {Screens} from '../navigation/RootNavigator';
-import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+import { Screens } from '../navigation/RootNavigator';
+import { launchImageLibrary } from 'react-native-image-picker';
+import ProgressBar from './ProgressBar';
 
-export const NewRecipeScreen1 = ({navigation}: {navigation: any}) => {
+export const NewRecipeScreen1 = ({ navigation }: { navigation: any }) => {
   const [images, setImages] = useState([]);
   const [currentStep, setCurrentStep] = useState(1);
 
@@ -28,7 +22,7 @@ export const NewRecipeScreen1 = ({navigation}: {navigation: any}) => {
     }
   };
 
-  const removeImage = (indexToRemove) => {
+  const removeImage = (indexToRemove: number) => {
     const updatedImages = images.filter((_, index) => index !== indexToRemove);
     setImages(updatedImages);
   };
@@ -44,9 +38,7 @@ export const NewRecipeScreen1 = ({navigation}: {navigation: any}) => {
       <ScrollView>
         <View style={{ padding: 30, minWidth: '100%' }}>
           <Text style={styles.titleText}>Nueva Receta</Text>
-          <Text style={styles.subTitleText}>
-            Información principal sobre tu receta
-          </Text>
+          <Text style={styles.subTitleText}>Información principal sobre tu receta</Text>
           <TextInput style={styles.input} placeholder="Título" />
           <TextInput style={styles.input} placeholder="Descripción" />
           <TextInput style={styles.input} placeholder="Link a video" />
@@ -65,14 +57,8 @@ export const NewRecipeScreen1 = ({navigation}: {navigation: any}) => {
             ) : (
               images.map((image, index) => (
                 <View key={index} style={styles.slide}>
-                  <Image
-                    source={{ uri: image }}
-                    style={styles.selectedImage}
-                  />
-                  <TouchableOpacity
-                    style={styles.deleteButton}
-                    onPress={() => removeImage(index)}
-                  >
+                  <Image source={{ uri: image }} style={styles.selectedImage} />
+                  <TouchableOpacity style={styles.deleteButton} onPress={() => removeImage(index)}>
                     <Text style={styles.deleteButtonText}>X</Text>
                   </TouchableOpacity>
                 </View>
@@ -80,35 +66,13 @@ export const NewRecipeScreen1 = ({navigation}: {navigation: any}) => {
             )}
           </Swiper>
 
-          <SmallButton
-            text="Seleccionar Imagen"
-            onPress={openGallery}
-          />
-
+          <SmallButton text="Seleccionar Imagen" onPress={openGallery} />
         </View>
       </ScrollView>
 
-      <PrimaryButton
-        text="Siguiente"
-        onPress={navigateToNextScreen}
-      />
-      <View style={styles.progressBar}>
-        <View style={[styles.progressStep, currentStep === 1 && styles.activeStep]}>
-          <Text style={[styles.stepText, currentStep === 1 && styles.activeStepText]}>1</Text>
-        </View>
-        <View style={styles.progressLine}></View>
-        <View style={styles.progressStep}>
-          <Text style={styles.stepText}>2</Text>
-        </View>
-        <View style={styles.progressLine}></View>
-        <View style={styles.progressStep}>
-          <Text style={styles.stepText}>3</Text>
-        </View>
-        <View style={styles.progressLine}></View>
-        <View style={styles.progressStep}>
-          <Text style={styles.stepText}>4</Text>
-        </View>
-      </View>
+      <ProgressBar totalSteps={4} currentStep={currentStep} />
+
+      <PrimaryButton text="Siguiente" onPress={navigateToNextScreen} />
       <View style={{ padding: 50 }}></View>
     </View>
   );
@@ -145,9 +109,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 10,
     right: 10,
-    backgroundColor: 'rgba(255, 99, 71, 0.8)', // Rojo pastel con opacidad
+    backgroundColor: 'rgba(255, 99, 71, 0.8)',
     padding: 10,
-    borderRadius: 50, // Hacer un círculo
+    borderRadius: 50,
     width: 30,
     height: 30,
     justifyContent: 'center',
@@ -156,37 +120,6 @@ const styles = StyleSheet.create({
   deleteButtonText: {
     color: 'white',
     fontSize: 14,
-  },
-  progressBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 20,
-    marginTop: 20,
-  },
-  progressStep: {
-    backgroundColor: '#797979', // Color gris por defecto
-    borderRadius: 50,
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  activeStep: {
-    backgroundColor: 'transparent', // Set the background to transparent
-  },
-  activeStepText: {
-    color: '#F37F21', // Color naranja para el paso activo
-    fontSize: 16,
-  },
-  progressLine: {
-    flex: 1,
-    height: 4,
-    backgroundColor: '#797979', // Color gris por defecto
-  },
-  stepText: {
-    color: 'white',
-    fontSize: 16,
   },
 });
 
---
+++ Foodie/src/components/ProgressBar.tsx
@@ -0,0 +1,85 @@
+// ProgressBar.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+interface ProgressBarProps {
+  totalSteps: number;
+  currentStep: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ totalSteps, currentStep }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.progressBar}>
+        {[...Array(totalSteps)].map((_, index) => (
+          <View key={index} style={styles.progressContainer}>
+            <View
+              style={[
+                styles.progressStep,
+                currentStep === index + 1 && styles.activeStep,
+              ]}
+            >
+              <Text
+                style={[
+                  styles.stepText,
+                  currentStep === index + 1 && styles.activeStepText,
+                ]}
+              >
+                {index + 1}
+              </Text>
+            </View>
+            {index < totalSteps - 1 && <View style={styles.progressLine} />}
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  progressBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  progressContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  progressStep: {
+    backgroundColor: '#797979',
+    borderRadius: 50,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activeStep: {
+    backgroundColor: '#F37F21',
+  },
+  activeStepText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  stepText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  progressLine: {
+    flex: 1,
+    height: 4,
+    backgroundColor: '#797979',
+    maxWidth: 80,
+  },
+});
+
+export default ProgressBar;
+
+
