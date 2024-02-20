9a3e66dfbe1d938f7faf5b5fc4cd3751fac6382b
pedromoura1 <55463700+pedromoura1@users.noreply.github.com>
2024-02-20 13:44:39 -0300

    Splash cambios

--- Foodie/App.tsx
+++ Foodie/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import React, { useEffect } from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
 import { ErrorHandler } from './src/components/Error/ErrorHandler';
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ErrorHandler>
-      <RootNavigator></RootNavigator>
+      <RootNavigator />
     </ErrorHandler>
   );
 };
+
 export default App;
