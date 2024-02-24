ace40d56870d341f0c7de41451f69048c2b1848d
alaniv <ivoalani@outlook.com>
2024-02-24 14:37:15 -0300

    Edit receta pantalla 4. Falta armar requests

--- Foodie/src/components/EditRecipeScreen4.tsx
+++ Foodie/src/components/EditRecipeScreen4.tsx
@@ -1,43 +1,223 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import {CommonStyle, Theme} from '../../Theme';
 import {PrimaryButton} from './PrimaryButton';
+import ProgressBar from './ProgressBar';
 import {Screens} from '../navigation/RootNavigator';
+import {useRoute} from '@react-navigation/native';
+import {postRecipeImages} from '../api/ApiFilesManager';
+import {postRecipe} from '../api/ApiRecipes';
+import {Recipe} from './FoodApiInterfaces/interfaces';
 
-export const EditRecipeScreen4 = ({navigation}: {navigation: any}) => {
+const TagsDropdown = ({availableTags, selectedTags, onTagSelect}: any) => {
+  const handleTagSelect = (tag: any) => {
+    if (!selectedTags.includes(tag)) {
+      onTagSelect([...selectedTags, tag]);
+    } else {
+      const updatedTags = selectedTags.filter(
+        (selectedTag: any) => selectedTag !== tag,
+      );
+      onTagSelect(updatedTags);
+    }
+  };
+
+  return (
+    <View>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap', marginTop: 16}}>
+        {availableTags.map((tag: any, index: any) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.selectedTag,
+              {
+                backgroundColor: selectedTags.includes(tag)
+                  ? '#F8A82E'
+                  : '#FFFFFF',
+              },
+            ]}
+            onPress={() => handleTagSelect(tag)}>
+            <Text
+              style={{
+                color: selectedTags.includes(tag) ? '#000000' : '#000000',
+              }}>
+              {tag}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export const EditRecipeScreen4 = ({navigation}: any) => {
+  const route: any = useRoute();
+
+  const [calories, setCalories] = useState(0);
+  const [proteins, setProteins] = useState(0);
+  const [totalFat, setTotalFat] = useState(0);
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const availableTags = [
+    'Vegana',
+    'Apta Celiacos',
+    'Rápida Preparación',
+    'Estimula el Sistema Inmune',
+    'Vegetarianas',
+    'Promueve la Flora Intestinal',
+    'Baja en Sodio',
+    'Baja en Carbohidratos',
+    'Antiinflamatoria',
+  ];
+
+  const handleTagSelect = (tags: any) => {
+    setSelectedTags(tags);
+  };
+  const submitFormEditRecipe = async () => {
+    let editRecipe = {
+      title: route.params.step1.title,
+      description: route.params.step1.description,
+      youtubeLink: route.params.step1.videoLink,
+      ingredients: route.params.step2.ingredients,
+      portions: route.params.step2.portions,
+      preparationTime: route.params.step2.preparationTime,
+      steps: route.params.step3.steps,
+      nutritionalProperties: {
+        calories: calories,
+        proteins: proteins,
+        totalFat: totalFat,
+      },
+      tags: selectedTags,
+    };
+    // route.params.imagesForDeletion // array con las imagenes que ya no estan asociadas a las recetas
+    /* //TODO(Ivo): PUT/PATCH de receta. POST Imagenes nuevas, DELETE imagenes viejas. ignorar las que ya estan subidas...
+    postRecipe(editRecipe)
+      .then(recipe => postRecipeImages(recipe._id, route.params.step1.images))
+      .catch(error => {
+        console.log(error);
+        navigation.navigate(Screens.ErrorScreen, {
+          errorCode: '3',
+          errorMessage: 'Error al crear receta, intente nuevamente',
+          nextScreen: Screens.Landing,
+        });
+      });
+      */
+
+    navigateToNextScreen();
+  };
+
+  useEffect(() => {
+    // initial values from recipe
+    const recipe: Recipe = route.params.recipe;
+    setCalories(recipe?.nutritionalProperties?.calories ?? 0);
+    setProteins(recipe?.nutritionalProperties?.proteins ?? 0);
+    setTotalFat(recipe?.nutritionalProperties?.totalFat ?? 0);
+    setSelectedTags(recipe?.tags ?? []);
+  }, [route.params?.recipe]);
+
+  const navigateToNextScreen = () => {
+    navigation.navigate(Screens.Landing);
+  };
+
   return (
     <View style={styles.background}>
-      <View style={styles.mainContainer}>
-        <Text style={styles.titleText}>Otros Datos</Text>
-        <Text style={styles.subTitleText}>
-          Agrega más información para que otros puedan encotrar tu receta
-          facilmente
-        </Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Cantidad de calorías"></TextInput>
-        <TextInput
-          style={styles.input}
-          placeholder="Cantidad de proteínas"></TextInput>
-        <TextInput
-          style={styles.input}
-          placeholder="Cantidad de grasas totales"></TextInput>
-        <TextInput
-          style={styles.input}
-          placeholder="Soy el dropdown de tags..."></TextInput>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.titleText}>Otros Datos</Text>
+          <Text style={styles.subTitleText}>
+            Agregá más información para que otros puedan encontrar tu receta
+            fácilmente
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Cantidad de calorías"
+            keyboardType="numeric"
+            defaultValue={calories.toString()}
+            onChangeText={newText => {
+              newText.replace(/[^0-9]/, '');
+              setCalories(Number.parseInt(newText));
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Cantidad de proteínas"
+            keyboardType="numeric"
+            defaultValue={proteins.toString()}
+            onChangeText={newText => {
+              newText.replace(/[^0-9]/, '');
+              setProteins(Number.parseInt(newText));
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Cantidad de grasas totales"
+            keyboardType="numeric"
+            defaultValue={totalFat.toString()}
+            onChangeText={newText => {
+              newText.replace(/[^0-9]/, '');
+              setTotalFat(Number.parseInt(newText));
+            }}
+          />
+
+          <TagsDropdown
+            availableTags={availableTags}
+            selectedTags={selectedTags}
+            onTagSelect={handleTagSelect}
+          />
+        </View>
+      </ScrollView>
+      <View style={{height: 160}}>
+        <ProgressBar currentStep={4} />
+        <View style={{height: 36}} />
+        <PrimaryButton
+          text="Finalizar"
+          backgroundColor={
+            calories && proteins && totalFat
+              ? Theme.colors.SECONDARY_2
+              : Theme.colors.NEUTRAL_3
+          }
+          onPress={() => {
+            if (calories && proteins && totalFat) submitFormEditRecipe();
+          }}
+        />
       </View>
-      <PrimaryButton
-        text="Finalizar"
-        onPress={() => navigation.navigate(Screens.Landing)}></PrimaryButton>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: CommonStyle.background,
-  mainContainer: CommonStyle.mainContainer,
+  background: {
+    backgroundColor: Theme.colors.PRIMARY_1,
+    flex: 1,
+    justifyContent: 'space-between', // Asegúrate de que el contenido se distribuya verticalmente
+    alignItems: 'center',
+  },
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  content: {
+    padding: 30,
+    minWidth: '100%',
+  },
   titleText: CommonStyle.titleText,
   subTitleText: CommonStyle.subTitleText,
   input: CommonStyle.input,
+  selectedTag: {
+    borderRadius: 15,
+    padding: 8,
+    margin: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 1,
+  },
 });
 
 export default EditRecipeScreen4;
