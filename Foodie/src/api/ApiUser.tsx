5f533ddafd6d4677f720928c2ea1154bad425ff0
German Gilabert <german.gilabert@gmail.com>
2024-02-25 19:03:05 -0300

    Added get user by id to usersApi

--- Foodie/src/api/ApiUser.tsx
+++ Foodie/src/api/ApiUser.tsx
@@ -4,6 +4,10 @@ export const getUser = async () =>
     await get("/users/")
             .catch((error) => { console.error("Error: ", error) });
 
+export const getUserById = async (profileId: string) => 
+    await get("/users/"+profileId)
+        .catch((error) => { console.error("Error: ", error) });
+
 export const putUser = async (userData: any) =>
     await put('/users/', userData)
         .catch((error) => { console.error("Error: ", error) });
