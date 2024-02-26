39c4c9bd81e192d92590d6c7d416c847715c834a
German Gilabert <german.gilabert@gmail.com>
2024-02-26 11:29:22 -0300

    fix upload multiple images on recipes

--- Foodie/src/api/ApiFilesManager.tsx
+++ Foodie/src/api/ApiFilesManager.tsx
@@ -40,13 +40,15 @@ let imgData: FormData = new FormData();
 
 export const postRecipeImages = async (recipeId: string, images: string[]) => 
 {
-    let imgData = await imgForm(recipeId, images);
-    try {
-        return await post('/recipes/' + recipeId + '/image', imgData);
-    }catch (error) 
-    {
-        console.error("Error: ", error);
-    }
+    images.forEach(async (img: string) => {
+        let imgData = await imgForm(recipeId, [img]);
+        try {
+            return await post('/recipes/' + recipeId + '/image', imgData);
+        }catch (error) 
+        {
+            console.error("Error: ", error);
+        }
+    });
 }
 
 export const postProfileImages = async (recipeId: string, images: string[]) => 
